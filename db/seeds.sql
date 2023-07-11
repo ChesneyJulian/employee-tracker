882ade759f3a606f04427906b62b1553e2a5c840
Chesney Julian <chesney.julian724@gmail.com>
2023-07-11 15:35:48 -0400

    add names to departments with seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE employe_db;
+
+INSERT INTO departments (name) VALUES 
+    ('Sales'), 
+    ('Customer Service'), 
+    ('Product Development'), 
+    ('Human Resources'), 
+    ('Recruitment');
