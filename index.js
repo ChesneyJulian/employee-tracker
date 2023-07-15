d88c56d717cb8d405942aab81bdea2d7381b72bc
Chesney Julian <chesney.julian724@gmail.com>
2023-07-15 17:16:46 -0400

    change if statements to switch case for speed and efficiency in handleQuery

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const addEmployee = [
     {
         type: "input",
         name: "first_name",
-        message: "Enter the name of the employee you wish to add:",
+        message: "Enter the first name of the employee you wish to add:",
     },
     {
         type: "input",
@@ -70,49 +70,61 @@ function runCommandLine() {
 }
 // function to handle answers to initial prompt sequence depending on what they selected
 function handleQuery(answers) {
-  // use conditionals to make corresponding prepared statements
-  if (answers.query === "View All Departments") {
-    // prepared statement to view all columns of departments table
-    db.query(`SELECT * FROM departments`, (err, results) => {
-      handleResults(err, results);
-    });
-  } else if (answers.query === "View All Roles") {
-    // prepared statement to view roles table joined with
-    // departments table for full info of each role including the department it is in
-    db.query(
-      `SELECT roles.id, roles.role_name AS title, departments.department_name AS department, roles.salary AS salary FROM roles LEFT JOIN departments ON roles.department_id = departments.id;`,
-      (err, results) => {
-        handleResults(err, results);
-      }
-    );
-  } else if (answers.query === "View All Employees") {
-    // prepared statement to view all employee info including role, department, salary,
-    // and manager name using joins to departments and roles as well as a self join
-    db.query(
-      `SELECT employee.id, employee.first_name, employee.last_name, roles.role_name AS title, departments.department_name AS department, roles.salary AS salary, manager.first_name as manager FROM employees employee LEFT JOIN employees manager ON manager.id = employee.manager_id LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id;`,
-      (err, results) => {
-        handleResults(err, results);
-      }
-    );
-  } else if (answers.query === "Add a Department") {
-    // if Add a Department is selected, run prompt module with question from addDepartment
-    prompt(addDepartment).then((answers) => {
-      // use answers to insert department into departments table
-      db.query(`INSERT INTO departments SET ?`, answers, (err, results) => {
-        handleResults(err, results);
-      });
-    });
-  } else if (answers.query === "Add an Employee") {
-    
-    prompt(addEmployee).then((answers) => {
+  // implement switch case to determine what actions to run in the case of each option 
+  switch (answers.query) {
+    case "View All Departments": {
+        // prepared statement to view all columns of departments table
+      db.query(`SELECT * FROM departments`, (err, results) => {
+          handleResults(err, results);
+    })
+      break;
+  }
+    case "View All Roles": {
+      // prepared statement to view roles table joined with
+      db.query(
+        `SELECT roles.id, roles.role_name AS title, departments.department_name AS department, roles.salary AS salary FROM roles LEFT JOIN departments ON roles.department_id = departments.id;`,
+        (err, results) => {
+          handleResults(err, results);
+        }
+      );
+      break;
+    }
+    case "View All Employees": {
+      // prepared statement to view all employee info including role, department, salary,
+      // and manager name using joins to departments and roles as well as a self join
+      db.query(
+        `SELECT employee.id, employee.first_name, employee.last_name, roles.role_name AS title, departments.department_name AS department, roles.salary AS salary, manager.first_name as manager FROM employees employee LEFT JOIN employees manager ON manager.id = employee.manager_id LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id;`,
+        (err, results) => {
+          handleResults(err, results);
+          }
+        );
+      break;
+    }
+    case "Add a Department": {
+      // if Add a Department is selected, run prompt module with question from addDepartment
+      prompt(addDepartment).then((answers) => {
+        // use answers to insert department into departments table with prepared statement
+        db.query(`INSERT INTO departments SET ?`, answers, (err, results) => {
+          handleResults(err, results);
+          });
+        });
+      break;
+    }
+    case "Add an Employee": {
+      // if Add an Employee is selected, run prompt module with questions from addEmployee
+      prompt(addEmployee).then((answers) => {
+        // use answers to insert employee into employees table with prepared statement
         db.query(`INSERT INTO employees SET ?`, answers, (err, results) => {
             handleResults(err, results);
-        });
-    });
-  } else if (answers.query === "Exit") {
-    // end prompt sequence if Exit is selected
-    process.exit();
-  }
+          });
+      });
+      break;
+    }
+    default: {
+      // end prompt sequence if Exit is selected (default of no other options selected)
+      process.exit();
+    } 
+  };
 }
 // function to handle all results and potential errors
 function handleResults(err, results) {
